589bed6dca5b88ffac078074df0618a5c98898f7
wpartin <djwest91@gmail.com>
2023-12-15 07:48:37 -0600

    [v0.1.1]

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,7 +4,8 @@ module "eks_cluster" {
   description = "test EKS cluster"
   enabled     = module.eks_cluster_label.enabled
   # enable_openid = true
-  name = module.eks_cluster_label.id_full
+  name   = module.eks_cluster_label.id_full
+  region = module.eks_cluster_label.region
   # subnet_ids    = ["subnet-01234567890abcdef"]
 
   tags = module.eks_cluster_label.tags
